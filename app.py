db5fa39e19d1f8e8ac541e9e3456f19350099c51
Alan Tseng <atseng202@gmail.com>
2020-12-02 12:21:35 -0800

    Get calc routes working

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+operator_mappings = {"add": add, "sub": sub, "mult": mult, "div": div}
+
+
+@app.route("/math/<operator>")
+def manipulate_nums(operator):
+    """ Handles addition of a and b from query params """
+    if operator not in operator_mappings:
+        return "<h1>NOT VALID OPERATION</h1>"
+
+    a = request.args["a"]
+    b = request.args["b"]
+
+    return f"{operator_mappings[operator](int(a), int(b))}"
